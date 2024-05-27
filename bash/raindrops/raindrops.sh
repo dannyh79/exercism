1f44f01254e3ca5e6b1096a3c758dfb90280d180
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-05-27 17:41:14 +0800

    [bash] Refactor raindrops

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -1,23 +1,9 @@
 #!/usr/bin/env bash
 
-output=$1
+(( $1 % 3 == 0 )) && output+="Pling"
+(( $1 % 5 == 0 )) && output+="Plang"
+(( $1 % 7 == 0 )) && output+="Plong"
 
-if (( $1 % 3 == 0 )); then
-  output="Pling"
-fi
-
-if (( $1 % 5 == 0 && output == $1 )); then
-  output="Plang"
-elif (( $1 % 5 == 0 && output != $1 )); then
-  output="${output}Plang"
-fi
-
-if (( $1 % 7 == 0 && output == $1 )); then
-  output="Plong"
-elif (( $1 % 7 == 0 && output != $1 )); then
-  output="${output}Plong"
-fi
-
-echo $output
+echo "${output:-$1}"
 
 exit 0
