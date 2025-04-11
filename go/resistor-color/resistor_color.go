e9fafb41585d760b5241010d1ddc76d9d6c40577
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2025-04-11 14:50:47 +0800

    [go] Submit resistor-color

---
+++ go/resistor-color/resistor_color.go
@@ -0,0 +1,37 @@
+package resistorcolor
+
+import "strings"
+
+// Colors returns the list of all colors.
+func Colors() []string {
+	return []string{
+		"black",
+		"brown",
+		"red",
+		"orange",
+		"yellow",
+		"green",
+		"blue",
+		"violet",
+		"grey",
+		"white",
+	}
+}
+
+var codeMap = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
+}
+
+// ColorCode returns the resistance value of the given color.
+func ColorCode(color string) int {
+	return codeMap[strings.ToLower(color)]
+}
